bd01be0295a5a5392dde7589b2ee7bfa40133004
SumedhaChaudhari <sumedhabsl@gmail.com>
2024-02-26 19:53:05 +0530

    front end code

--- mess_portal/src/App.js
+++ mess_portal/src/App.js
@@ -1,13 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
-import Subscribecomp from './Components/Subscribecomp';
-import Menuupdatecomp from './Components/Menuupdatecomp';
-import vendorsignupcomp, { Vendorsignupcomp } from './Components/vendorsignupcomp';
-import VendorsComp from './Components/VendorsComp';
-import IndividualVendorComp from './Components/IndividualVendorComp';
-import CustomerDeliveryComp from './Components/CustomerDeliveryComp';
-import RouterComp from './Components/RouterComp';
+
 import RoutingComp from './Components/RoutingComp';
 
 function App() {
